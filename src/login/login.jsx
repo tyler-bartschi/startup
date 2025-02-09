ad258b706b13aa9ac4cec622919148c71422ae77
tyler-bartschi <tylerabartschi@gmail.com>
2025-02-09 16:05:20 -0700

    Updated login.jsx

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -3,9 +3,23 @@ import "./login.css"
 
 export function Login() {
     return (
-        <main>
-            <div>
-                Login displayed here
+        <main className="container-fluid text-center">
+            <h1 className="welcome">Welcome to <span className="welcome-title">badreads</span>!</h1>
+            <div className="login-box">
+                <form method="get" action="home.html">
+                    <div className="input-group mb-3 pt-3">
+                        <span className="input-group-text">Username</span>
+                        <input className="form-control" type="text" placeholder="your username" />
+                    </div>
+                    <div className="input-group mb-3">
+                        <span className="input-group-text">Password</span>
+                        <input className="form-control" type="password" placeholder="password" />
+                    </div>
+                    <div className="btn-wrapper">
+                        <button type="submit" className="btn">Login</button>
+                        <button type="submit" className="btn create-btn">Create</button>
+                    </div>
+                </form>
             </div>
         </main>
     );
