ac0f02c025ac2464b0bff6522b2e7a81652e97e0
tyler-bartschi <tylerabartschi@gmail.com>
2025-02-21 21:13:07 -0700

    Updated scoreTable to dynamically render the scores

--- src/reviews/scoreTable.jsx
+++ src/reviews/scoreTable.jsx
@@ -8,8 +8,16 @@ export function ScoreTable() {
     
 
     if (scoresText) {
-        setScores(JSON.parse(scoresText));
-
+        let raw_scores = JSON.parse(scoresText);
+        setScores(countScores());
+        for (let i = 5; i > 0; i--) {
+            scoreRows.push(
+                <tr key={i}>
+                    <td><span className="star">{"★".repeat(i)}</span></td>
+                    <td>{scores[i - 1]}</td>
+                </tr>
+            );
+        }
     } else {
         for (let i = 5; i > 0; i--) {
             scoreRows.push(
@@ -27,28 +35,19 @@ export function ScoreTable() {
         </table>
         
     );
+
+    function countScores() {
+        let counted_scores = [];
+        for (let i = 1; i < 6; i++) {
+            let count = 0;
+            for (const item of raw_scores) {
+                if (item.score == i) {
+                    count++;
+                }
+            }
+            counted_scores.push(count);
+        }
+        return counted_scores;
+    }
+
 }
-
-
-{/* <table className="data-table" >
-                            <tr>
-                                <td><span className="star">★★★★★</span></td>
-                                <td>3</td>
-                            </tr>
-                            <tr>
-                                <td><span className="star">★★★★</span></td>
-                                <td>1</td>
-                            </tr>
-                            <tr>
-                                <td><span className="star">★★★</span></td>
-                                <td>1</td>
-                            </tr>
-                            <tr>
-                                <td><span className="star">★★</span></td>
-                                <td>0</td>
-                            </tr>
-                            <tr>
-                                <td><span className="star">★</span></td>
-                                <td>0</td>
-                            </tr>
-                        </table> */}
