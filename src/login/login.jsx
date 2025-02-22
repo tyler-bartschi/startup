6d21a14882dbbcc203772b3b4d8e94d49491a9f3
tyler-bartschi <tylerabartschi@gmail.com>
2025-02-21 19:20:53 -0700

    Unauthenticated component

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import "./login.css"
+import "./login.css";
+
+import {Unauthenticated} from "./unauthenticated";
+import {Authenticated} from "./authenticated";
+import {AuthState} from "./authState";
+
 
 export function Login({ userName, authState, onAuthChange}) {
     return (
         <main className="container-fluid text-center">
             <h1 className="welcome">Welcome to <span className="welcome-title">badreads</span>!</h1>
-            
+            {authState === AuthState.Authenticated && (
+                <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+            )}
+
+            {authState === AuthState.Unauthenticated && (
+                <Unauthenticated userName={userName} onLogin={(loginUserName) => onAuthChange(loginUserName, AuthState.Authenticated)} />
+            )}
         </main>
     );
 }
 
 
-<div className="login-box">
+            <div className="login-box">
                 <div>
                     <div className="input-group mb-3 pt-3">
                         <span className="input-group-text">Username</span>
--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 
 
-export function Unauthenicated(props){
+export function Unauthenticated(props){
+    const [userName, setUserName] = React.useState(props.userName);
+    const [password, setPassword] = React.useState('');
+    
+    async function loginUser() {
+        localStorage.setItem('userName', userName);
+        props.onLogin(userName);
+    }
+
     return (
-        <p>Unauthenicated screen</p>
+        <div className="login-box">
+            <div>
+                <div className="input-group mb-3 pt-3">
+                    <span className="input-group-text">Username</span>
+                    <input className="form-control" type="text" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="your username" />
+                </div>
+                <div className="input-group mb-3">
+                    <span className="input-group-text">Password {'\u200B'}</span>
+                    <input className="form-control" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="password" />
+                </div>
+                <div className="btn-wrapper">
+                    <button type="submit" className="btn" onClick={() => loginUser()} disabled={!userName || !password} >Login</button>
+                    <button type="submit" className="btn create-btn" onClick={() => loginUser()} disabled={!userName || !password}>Create</button>
+                </div>
+            </div>
+        </div>
     );
 }
